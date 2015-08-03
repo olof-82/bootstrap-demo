1eadd521bee0f1b07ac367162488f1ab8f232790
Olof Persson <olof.82@gmail.com>
2015-08-03 11:42:11 +0200

    Initial commit after running yo...
    Change-Id: Ic8548ae3675ebf929cfd1ab0396e4169559447f3

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name bootstrapDemoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the bootstrapDemoApp
+ */
+angular.module('bootstrapDemoApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
